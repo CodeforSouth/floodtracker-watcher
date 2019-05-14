4abf0b913e4bc11e4a0266f7b4f65eabe2122f2d
Bryce Kerley <bkerley@brycekerley.net>
2019-05-13 20:35:31 -0400

    levels controller

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,29 @@
+class LevelsController < ApplicationController
+  def index
+    @sparks = Spark.connection.select_all(<<-SQL)
+SELECT
+        windows.date,
+        s.coreid,
+        MIN(cast(s.data AS FLOAT)),
+
+        AVG(cast(s.data AS FLOAT)),
+
+        STDDEV(cast(s.data AS FLOAT)),
+        MAX(cast(s.data AS FLOAT))
+FROM
+        (SELECT DATE(generate_series(
+                (SELECT MIN(DATE(published_at)) FROM sparks),
+                (SELECT MAX(DATE(published_at)) FROM sparks),
+                '1 day')) AS date) as windows
+
+RIGHT JOIN
+      sparks as s
+      ON date(s.published_at) = windows.date
+WHERE
+        s.event = 'floodtracker/level_mm'
+GROUP BY
+      windows.date,
+      s.coreid;
+SQL
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sparks
   resources :events
+
+  resources :levels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
