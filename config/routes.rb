f155674e2795fb8ee99f3873429eb3415b21c11e
Bryce Kerley <bkerley@brycekerley.net>
2019-05-18 10:37:09 -0400

    fix sparks route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :events
   resources :levels
 
-  resource :sparks
+  resources :sparks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
