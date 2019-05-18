c8c2560a3c5356b8ecb97c2c773bba1b40fac12f
Bryce Kerley <bkerley@brycekerley.net>
2019-05-18 15:41:19 -0400

    roll up level_raws and spark_diagnostics

--- app/models/spark_diagnostic.rb
+++ app/models/spark_diagnostic.rb
@@ -6,4 +6,9 @@ class SparkDiagnostic < ApplicationRecord
       published_at: params[:published_at]
     )
   end
+
+  def self.rollup
+    where("published_at < (now() - '7 day'::interval)").
+      delete_all
+  end
 end
