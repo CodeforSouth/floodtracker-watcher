39e122d6b75a5aed5bb165b4cb8dfd78d155931b
Bryce Kerley <bkerley@brycekerley.net>
2019-05-19 22:11:11 -0400

    start level rollup

--- app/models/level.rb
+++ app/models/level.rb
@@ -6,4 +6,45 @@ class Level < ApplicationRecord
       published_at: params[:published_at]
     )
   end
+
+  def self.rollup
+    connection.execute(<<-SQL)
+INSERT INTO level_histories
+(date, coreid, "count", first_publish, last_id, min_reading, mean_reading,
+stddev_reading, max_reading, first_reading, created_at, updated_at)
+(SELECT i.*,
+        t.reading AS first_reading,
+        now(), now()
+FROM (
+    SELECT
+            windows.date,
+            s.coreid,
+            COUNT(s.reading),
+            MIN(s.published_at) AS first_publish,
+            MAX(s.id) AS last_id,
+            MIN(s.reading) as min_reading,
+            AVG(s.reading) as mean_reading,
+            STDDEV(s.reading) as stddev_reading,
+            MAX(s.reading) as max_reading
+    FROM
+            (SELECT DATE(generate_series(
+                    (SELECT MIN(DATE(published_at)) FROM levels),
+                    (SELECT MAX(DATE(published_at)) FROM levels
+                        WHERE published_at < NOW() - '8 d'::interval
+                    ),
+                    '1 day')) AS date) AS windows
+
+    INNER JOIN
+        levels AS s ON date(s.published_at) = windows.date
+    GROUP BY
+        windows.date, s.coreid
+    ORDER BY
+        windows.date asc, s.coreid asc) AS i
+INNER JOIN
+        levels AS t ON
+                t.coreid = i.coreid AND
+                t.published_at = i.first_publish
+);
+SQL
+  end
 end
