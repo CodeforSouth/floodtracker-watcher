86697acd63f3e4f33927314d68cefd05f9e8bf7b
Bryce Kerley <bkerley@brycekerley.net>
2019-07-22 20:44:26 -0400

    start devices views

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -12,55 +12,6 @@ class DevicesController < ApplicationController
   def show
   end
 
-  # GET /devices/new
-  def new
-    @device = Device.new
-  end
-
-  # GET /devices/1/edit
-  def edit
-  end
-
-  # POST /devices
-  # POST /devices.json
-  def create
-    @device = Device.new(device_params)
-
-    respond_to do |format|
-      if @device.save
-        format.html { redirect_to @device, notice: 'Device was successfully created.' }
-        format.json { render :show, status: :created, location: @device }
-      else
-        format.html { render :new }
-        format.json { render json: @device.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /devices/1
-  # PATCH/PUT /devices/1.json
-  def update
-    respond_to do |format|
-      if @device.update(device_params)
-        format.html { redirect_to @device, notice: 'Device was successfully updated.' }
-        format.json { render :show, status: :ok, location: @device }
-      else
-        format.html { render :edit }
-        format.json { render json: @device.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /devices/1
-  # DELETE /devices/1.json
-  def destroy
-    @device.destroy
-    respond_to do |format|
-      format.html { redirect_to devices_url, notice: 'Device was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_device
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,13 @@
 class Device < ApplicationRecord
   self.primary_key = 'id'
+
+  RELATED_TABLES = %w{batteries battery_histories
+    levels level_histories leveL_raws
+    quips
+    sleep_plans sleep_plan_histories
+    sparks spark_diagnostics}
+
+  RELATED_TABLES.each do |t|
+    has_many t.to_sym, foreign_key: 'coreid'
+  end
 end
